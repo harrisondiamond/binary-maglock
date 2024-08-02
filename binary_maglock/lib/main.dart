5bfe3ec7d0ab183041e4e1d0f79125abf9b934c8
Harrison Diamond <harrison@thediamonds.me>
2024-08-02 09:42:16 -0500

    original ui to main

--- binary_maglock/lib/main.dart
+++ binary_maglock/lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Binary Maglock Redux',
       theme: _buildTheme(Brightness.dark),
 
       // ThemeData(
